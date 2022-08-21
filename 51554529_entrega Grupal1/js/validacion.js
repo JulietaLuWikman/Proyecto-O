29177075af614594f148e52e0186860a873c5daf
Julieta Lu Wikman <110486009+JulietaLuWikman@users.noreply.github.com>
2022-08-21 00:59:41 -0300

    Add files via upload

---
+++ 51554529_entrega Grupal1/js/validacion.js
@@ -0,0 +1,25 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+ const nombre = document.getElementById("nombre");
+ const apellido = document.getElementById("apellido");
+ const email = document.getElementById("email");
+ const password1 = document.getElementById("password1");
+ const password2 = document.getElementById("password2");
+ const checkbox = document.getElementById("terminos");
+ const boton = document.getElementById("regBtn");
+
+ boton.addEventListener("click", function(){
+    if (nombre.value == "" || apellido.value == "" || email.vale == "" ||
+     !checkbox.checked || password1.value != password2.value || password1.value.length < 6) {
+        showAlertError()
+     }
+     else{
+        showAlertSuccess
+     }
+ });
